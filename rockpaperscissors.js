312391f3cb8cfe38d9dc93ffc80aa403c80d406c
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-01-27 20:59:03 +0100

    Play a game by written user input (promp method)

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,13 +1,44 @@
 function getComputerChoice() {
-    let choice;
     let num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
-        return "Rock";
+        return "rock";
     } else if (num === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playGame(playerSelection, computerSelection) {
+    playerSelection = prompt("Player's turn:");
+    computerSelection = getComputerChoice();
+    playerSelection = playerSelection.toLowerCase();
+
+    // Computer chooses scissors
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        confirm("You won! Rock beats Scissors");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        confirm("You lose! Scissors beats Paper");
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        confirm("Draw! Both players choose Scissors");
+    // Computer chooses paper
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        confirm("You lose! Paper beats Rock");
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        confirm("Draw! Both players choose Paper");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        confirm("You won! Scissors beat Paper");
+    // Computer chooses rock
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        confirm("Draw! Both players choose Rock");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        confirm("You won! Paper beat Rock");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        confirm("You lose! Rock beats Scissors");
+    // Something failed
+    } else {
+        confirm("Write a valid option");
+    }
+}
+
+playGame();
