29f2c1e2ee43dd11130d09f549aa7ecb2b89a094
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-01-30 15:49:54 +0100

    Set the loop for a game with infinite rounds (no condition breaks the loop)

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,9 +9,9 @@ function getComputerChoice() {
     }
 }
 
-function playGame(playerSelection, computerSelection) {
-    playerSelection = prompt("Player's turn:");
-    computerSelection = getComputerChoice();
+function playRound() {
+    let playerSelection = prompt("Write your choice:");
+    let computerSelection = getComputerChoice();
     playerSelection = playerSelection.toLowerCase();
 
     // Computer chooses scissors
@@ -21,24 +21,48 @@ function playGame(playerSelection, computerSelection) {
         confirm("You lose! Scissors beats Paper");
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         confirm("Draw! Both players choose Scissors");
-    // Computer chooses paper
+        // Computer chooses paper
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         confirm("You lose! Paper beats Rock");
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         confirm("Draw! Both players choose Paper");
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         confirm("You won! Scissors beat Paper");
-    // Computer chooses rock
+        // Computer chooses rock
     } else if (playerSelection === "rock" && computerSelection === "rock") {
         confirm("Draw! Both players choose Rock");
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         confirm("You won! Paper beat Rock");
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         confirm("You lose! Rock beats Scissors");
-    // Something failed
+        // Something failed
     } else {
         confirm("Write a valid option");
     }
 }
 
-playGame();
+function game() {
+    let playerWins = 0,
+    computerWins = 0,
+    draws = 0,
+    rounds = 0;
+
+    for (rounds = 0; playerWins <= 5 || computerWins <= 5; rounds++) {
+        playRound();
+        rounds++;
+        if (playerWins === 5) {
+            confirm("You won 5 matches!");
+        } else if (computerWins === 5) {
+            confirm("Computer won 5 matches!");
+        } else {
+            confirm(
+                `Player wins: ${playerWins}
+            Computer wins: ${computerWins}
+            Draws: ${draws}
+            Rounds played: ${rounds}`);
+        }
+    }
+
+}
+
+game();
