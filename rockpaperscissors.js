542722f81defa263bccb0b6b860635af014c4d27
miguel-maps <77571745+Miguel-maps@users.noreply.github.com>
2023-02-15 12:12:59 +0100

    Update all scores when a match is played

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,80 +1,77 @@
+let playerWins = 0;
+let computerWins = 0;
+let drawCounter = 0;
+let rounds = 0;
+
 function getComputerChoice() {
     return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
-  }
+}
 
-  function playRound(playerSelection) {
+function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
 
     // Computer chooses scissors
     if (playerSelection === "rock" && computerSelection === "scissors") {
         alert("You won! Rock beats Scissors");
         playerWins++;
-        rounds++;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         alert("You lose! Scissors beats Paper");
         computerWins++;
-        rounds++;
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         alert("Draw! Both players choose Scissors");
-        draws++;
-        rounds++;
+        drawCounter++;
         // Computer chooses paper
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         alert("You lose! Paper beats Rock");
         computerWins++;
-        rounds++;
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         alert("Draw! Both players choose Paper");
-        draws++;
-        rounds++;
+        drawCounter++;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         alert("You won! Scissors beat Paper");
         playerWins++;
-        rounds++;
         // Computer chooses rock
     } else if (playerSelection === "rock" && computerSelection === "rock") {
         alert("Draw! Both players choose Rock");
-        draws++;
-        rounds++;
+        drawCounter++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         alert("You won! Paper beat Rock");
         playerWins++;
-        rounds++;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         alert("You lose! Rock beats Scissors");
         computerWins++;
-        rounds++;
     }
+    rounds++;
+    updateAllCounters();
 }
 
-function game() {
-    let playerWins = 0,
-    computerWins = 0,
-    draws = 0,
-    rounds = 0;
-/*
-    for (let counter = 0; playerWins <= 5 || computerWins <= 5; counter++) {
-        playRound();
-        if (playerWins === 5) {
-            alert("You won 5 matches!");
-            break;
-        } else if (computerWins === 5) {
-            alert("Computer won 5 matches!");
-            break;
-        } else {
-            alert(`Player wins: ${playerWins}
-            Computer wins: ${computerWins}
-            Draws: ${draws}
-            Rounds played: ${rounds}`);
-        }
-    }
-    */
-
-    
-
+// Update every counter
+function updateAllCounters() {
+    updatePlayerWins();
+    updateComputerWins();
+    updateDraws();
+    updateRounds();
 }
 
-// game();
+function updateRounds() {
+    const totalRounds = document.querySelector('#totalRounds');
+    totalRounds.textContent = rounds;
+}
+
+function updatePlayerWins() {
+    const playerScore = document.querySelector('#playerScore');
+    playerScore.textContent = playerWins;
+}
+
+function updateComputerWins() {
+    const computerScore = document.querySelector('#computerScore');
+    computerScore.textContent = computerWins;
+}
+
+function updateDraws() {
+    const draws = document.querySelector('#draws');
+    draws.textContent = drawCounter;
+}
 
 /*
 
@@ -82,6 +79,7 @@ DOM
 
 */
 
+// Player buttons
 const playerButtonRock = document.querySelector('#rock');
 const playerButtonPaper = document.querySelector('#paper');
 const playerButtonScissors = document.querySelector('#scissors');
